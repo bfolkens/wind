b5ba4e8b0aa53395c164b5027c6a5fdd81d37fc2
Bradford Folkens <i.am@bradfolkens.com>
2023-03-17 16:00:20 -0500

    vbump

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Wind.MixProject do
   def project do
     [
       app: :wind,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       erlc_paths: erlc_paths(Mix.env()),
