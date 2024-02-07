05451beb2a5f19c55bcff22e7a754cda1c671197
Bradford Folkens <i.am@bradfolkens.com>
2024-02-07 09:24:33 -0600

    Added better error handling - allow user to override default {:stop, ...} - Closes #3

--- lib/wind/client.ex
+++ lib/wind/client.ex
@@ -30,15 +30,28 @@ defmodule Wind.Client do
   Invoked after a connection is established. Override to setup post-connection state.
   """
   @callback handle_connect(state :: term) ::
-              {:reply, frame :: frame(), new_state :: term}
-              | {:noreply, new_state :: term}
+              {:reply, frame :: frame(), new_state}
+              | {:noreply, new_state}
+              | {:stop, reason, new_state}
+            when new_state: term, reason: term
 
   @doc """
   Invoked for each received frame.
   """
   @callback handle_frame(frame :: frame(), state :: term) ::
-              {:reply, frame :: frame(), new_state :: term}
-              | {:noreply, new_state :: term}
+              {:reply, frame :: frame(), new_state}
+              | {:noreply, new_state}
+              | {:stop, reason, new_state}
+            when new_state: term, reason: term
+
+  @doc """
+  Invoked upon error. Override to handle error state.
+  """
+  @callback handle_error(reason :: any, state :: term) ::
+              {:reply, frame :: frame(), new_state}
+              | {:noreply, new_state}
+              | {:stop, reason, new_state}
+            when new_state: term, reason: term
 
   def start_link(module, default, options \\ []) when is_list(default) do
     GenServer.start_link(module, default, options)
@@ -73,7 +86,7 @@ defmodule Wind.Client do
         # Don't include TLS options when using non-TLS connection
         # or the user will encounter a `:badarg` from `:gen_tcp`.
         default_transport_opts =
-          uri.scheme == "wss" && [verify: :verify_none] || []
+          (uri.scheme == "wss" && [verify: :verify_none]) || []
 
         http_opts =
           Keyword.get(opts, :http_opts,
@@ -89,16 +102,18 @@ defmodule Wind.Client do
             {:noreply, %{state | conn_info: {conn, ref, nil}}}
 
           {:error, reason} ->
-            {:stop, {:error, reason}, state}
+            handle_error(reason, state)
 
           {:error, conn, reason} ->
-            {:stop, {:error, conn, reason}, state}
+            handle_error(reason, %{state | conn_info: {conn, nil, nil}})
         end
       end
 
       defp maybe_handle_reply({:noreply, state}), do: {:noreply, state}
 
       defp maybe_handle_reply({:reply, message, state}), do: send_frame(message, state)
+
+      defp maybe_handle_reply({:stop, message, state}), do: {:stop, message, state}
 
       defp send_frame(message, %{conn_info: {conn, ref, websocket}} = state) do
         case Wind.send(conn, ref, websocket, message) do
@@ -149,38 +164,61 @@ defmodule Wind.Client do
             %{conn_info: {conn, ref, _}} = state
           ) do
         Logger.debug(fn -> "Upgrading to websocket" end)
-        {:ok, conn, ref, websocket, _response} = Wind.setup(conn, ref, http_reply_message)
-        state = %{state | conn_info: {conn, ref, websocket}}
 
-        unquote do
-          if opts[:ping_timer] do
-            quote do
-              start_ping_timer()
+        case Wind.setup(conn, ref, http_reply_message) do
+          {:ok, conn, ref, websocket, _responses} ->
+            state = %{state | conn_info: {conn, ref, websocket}}
+
+            unquote do
+              if opts[:ping_timer] do
+                quote do
+                  start_ping_timer()
+                end
+              end
             end
-          end
-        end
 
-        state
-        |> handle_connect()
-        |> maybe_handle_reply()
+            state
+            |> handle_connect()
+            |> maybe_handle_reply()
+
+          {:error, conn, reason, _responses} ->
+            handle_error(reason, %{state | conn_info: {conn, ref, nil}})
+
+          :unknown ->
+            handle_error(:unknown, state)
+        end
       end
+
+      @impl true
+      def handle_info({:error, reason}, state), do: handle_error(reason, state)
 
       @impl true
       def handle_info(message, %{conn_info: {conn, ref, websocket}} = state)
           when not is_nil(conn) and not is_nil(ref) and not is_nil(websocket) do
-        {:ok, conn, websocket, data} = Wind.decode(conn, ref, websocket, message)
-        state = %{state | conn_info: {conn, ref, websocket}}
+        case Wind.decode(conn, ref, websocket, message) do
+          {:ok, conn, websocket, data} ->
+            state = %{state | conn_info: {conn, ref, websocket}}
 
-        state =
-          Enum.reduce(data, state, fn datum, state ->
-            {:noreply, state} =
-              handle_frame(datum, state)
-              |> maybe_handle_reply()
+            state =
+              Enum.reduce(data, state, fn datum, state ->
+                {:noreply, state} =
+                  handle_frame(datum, state)
+                  |> maybe_handle_reply()
 
-            state
-          end)
+                state
+              end)
 
-        {:noreply, state}
+            {:noreply, state}
+
+          {:error, conn, reason, _responses} ->
+            handle_error(reason, %{state | conn_info: {conn, ref, websocket}})
+
+          {:error, websocket, reason} ->
+            handle_error(reason, %{state | conn_info: {conn, ref, websocket}})
+
+          {:error, reason} ->
+            handle_error(reason, state)
+        end
       end
 
       @impl true
@@ -189,9 +227,16 @@ defmodule Wind.Client do
       defoverridable handle_info: 2
 
       @doc false
+      @impl true
       def handle_connect(state), do: {:noreply, state}
 
       defoverridable handle_connect: 1
+
+      @doc false
+      @impl true
+      def handle_error(reason, state), do: {:stop, {:error, reason}, state}
+
+      defoverridable handle_error: 2
     end
   end
 end
